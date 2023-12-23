7891b8dfe6d3206d5aebb50c4acfbf65ca3de135
Olga Meredova <olya.gurbo@gmail.com>
2023-12-23 17:17:24 +0300

    docs: add the last solution but with 2 mistakes, look for full solution

--- src/what-season.js
+++ src/what-season.js
@@ -12,23 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  // if (!date) {
-  //   return 'Unable to determine the time of year!';
-  // } else if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime())) {
-  //   throw new Error('Invalid date!');
-  // } 
-  // const month = date.getMonth();
+  if (!date) {
+    throw new Error('Invalid date!');
+  }
 
-  // if (month >= 0 && month <= 1 || month === 11) {
-  //   return 'winter';
-  // } else if (month >= 2 && month <= 4) {
-  //   return 'spring';
-  // } else if (month >= 5 && month <= 7) {
-  //   return 'summer';
-  // } else if (month >= 8 && month <= 10) {
-  //   return 'autumn';
-  // }
-  throw new NotImplementedError('Not implemented');
+  try {
+    const month = date.getMonth();
+
+    if (isNaN(month) || !isFinite(month)) {
+      throw new Error('Invalid date!');
+    }
+
+    switch (true) {
+      case month >= 0 && month <= 1 || month === 11:
+        return 'winter';
+      case month >= 2 && month <= 4:
+        return 'spring';
+      case month >= 5 && month <= 7:
+        return 'summer';
+      case month >= 8 && month <= 10:
+        return 'autumn';
+      default:
+        throw new Error('Invalid date!');
+    }
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
 }
 
 
